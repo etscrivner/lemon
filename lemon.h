08c530a366f989ccc15dc82df0dec729156e3520
Eric Scrivner <zenogais@gmail.com>
2010-02-23 02:46:09 -0800

    Removed the cpp file so now lemon is just a small header. Created a README.

--- lemon.h
+++ lemon.h
@@ -2,7 +2,7 @@
 // Lemon Unit Test Framework
 // Author: Eric Scrivner
 //
-// Time-stamp: <Last modified 2010-02-23 02:21:13 by Eric Scrivner>
+// Time-stamp: <Last modified 2010-02-23 02:38:14 by Eric Scrivner>
 //
 // Description:
 //   A lightweight, minimal unit-testing framework based on Perl Test::More
@@ -29,13 +29,37 @@ public:
   // This simply lets lemon know how many tests you're planning to run so that
   // it can properly output the diagnostic information and doesn't have to
   // count by hand (which can be tricky as one test can have many assertions).
-  Lemon(unsigned int num_planned_tests);
+  Lemon(unsigned int num_planned_tests)
+  : num_tests_(0),
+    test_number_(0),
+    num_skipped_(0),
+    num_failed_(0)
+  {
+    std::cout << "1.." << num_planned_tests << std::endl;
+  }
   
   //////////////////////////////////////////////////////////////////////////////
   // Function: end
   //
   // Signifies the end of the testing phase and prints the results.
-  void end();
+  void end() {
+      // If any tests were skipped
+    if (num_skipped_ > 0) {
+      // Display information about the skipped tests
+      std::cout << "# Looks like you planned " << num_tests_;
+      std::cout << " but only ran " << (num_tests_ - num_skipped_) << ".\n";
+    }
+    
+    // If any tests were failed
+    if (num_failed_ > 0) {
+      // Display test failure statistics
+      std::cout << "# Looks like you failed " << num_failed_;
+      std::cout << " of " << num_tests_ << " tests.\n";
+    } else {
+      // Otherwise display success message
+      std::cout << "# Looks like you passed all " << num_tests_ << " tests.\n";
+    }
+  }
   
   //////////////////////////////////////////////////////////////////////////////
   // Function: diag
@@ -44,7 +68,9 @@ public:
   //    message - A string to be written out to the display
   //
   // Used to display diagnostic information which is not a unit test.
-  void diag(const std::string& message);
+  void diag(const std::string& message) {
+    std::cout << "# " << message << std::endl;
+  }
   
   //////////////////////////////////////////////////////////////////////////////
   // Function: ok
@@ -55,7 +81,31 @@ public:
   //
   // Marks this test as passed if pass is true.  The test is marked as
   // failing otherwise.
-  bool ok(bool passed, const std::string& test_name);
+  bool ok(bool passed, const std::string& test_name) {
+    // Increment the number of tests run
+    num_tests_++;
+    
+    // If this is a skip or todo message
+    if (test_name[0] != '#') {
+      // Not the safest thing, but append a dash to the front
+      const_cast<std::string&>(test_name) = "- " + test_name;
+    }
+    
+    // If the test was passed
+    if (passed) {
+      // Inform you that the test passed
+      std::cout << "ok " << num_tests_ << " " << test_name << "\n";
+    } else {
+      // Otherwise increment the number of failed tests
+      num_failed_++;
+      
+      // Inform you that the test failed
+      std::cout << "not ok " << num_tests_ << " " << test_name << "\n";
+      diag("  Test failed, expected <true> but was <false>");
+    }
+  
+    return passed;
+  }
   
   //////////////////////////////////////////////////////////////////////////////
   // Function: not_ok
@@ -66,7 +116,9 @@ public:
   //
   // Marks this test as passed if the boolean parameter is false. The test is 
   // marked as failing otherwise.
-  bool not_ok(bool failed, const std::string& test_name);
+  bool not_ok(bool failed, const std::string& test_name) {
+    return ok(!failed, test_name);
+  }
 
   //////////////////////////////////////////////////////////////////////////////
   // Function: is
@@ -129,8 +181,10 @@ public:
   // Parameters:
   //    test_name - A short, descriptive name for this test
   //
-  // Marks the given test as passing without actually testing anything.
-  bool pass (const std::string& test_name);
+  // Marks the given test as trivially passing.
+  bool pass (const std::string& test_name) {
+    return ok(true, test_name);
+  }
 
   //////////////////////////////////////////////////////////////////////////////
   // Function: fail
@@ -138,8 +192,10 @@ public:
   // Parameters:
   //    test_name - A short, descriptive name for this test
   //
-  // Marks the given test as failing without actually testing anything.
-  bool fail (const std::string& test_name);
+  // Marks the given test as trivially failing.
+  bool fail (const std::string& test_name) {
+    return ok(false, test_name);
+  }
 
   //////////////////////////////////////////////////////////////////////////////
   // Function: skip
@@ -149,7 +205,13 @@ public:
   //    num_to_skip - The number of tests to skip
   //
   // Skips the given number of tests adding them to the skip count.
-  void skip (const std::string& reason, unsigned int num_to_skip);
+  void skip (const std::string& reason, unsigned int num_to_skip) {
+    num_skipped_ += num_to_skip;
+  
+    for (unsigned int i = 0; i < num_to_skip; i++) {
+      pass("# SKIP " + reason);
+    }	
+  }
 
   //////////////////////////////////////////////////////////////////////////////
   // Function: todo
@@ -158,12 +220,15 @@ public:
   //    what - What needs to be done
   //
   // Prints a message indicating what is left to be done
-  void todo (const std::string& what);
+  void todo (const std::string& what) {
+    num_skipped_++;
+    pass("# TODO " + what);
+  }
 private:
-  unsigned int num_tests; // The total number of tests to be executed
-  unsigned int test_number; // The number of the current test
-  unsigned int num_skipped; // The number of tests marked as skipped
-  unsigned int num_failed; // The number of tests marked as failing
+  unsigned int num_tests_; // The total number of tests to be executed
+  unsigned int test_number_; // The number of the current test
+  unsigned int num_skipped_; // The number of tests marked as skipped
+  unsigned int num_failed_; // The number of tests marked as failing
 };
 
 #endif // LEMON_H__
