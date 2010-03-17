c9a4b7c3dbf365685a1e2bd166d5e3b046f35d66
Eric Scrivner <zenogais@gmail.com>
2010-03-17 10:17:00 -0700

    More copyright notices.

--- examples/factorial_test.cpp
+++ examples/factorial_test.cpp
@@ -1,10 +1,31 @@
 ///////////////////////////////////////////////////////////////////////////////
 // factorial_test.cpp - A simple factorial example
 //
-// Time-stamp: <Last modified 2010-03-03 01:29:58 by Eric Scrivner>
+// Time-stamp: <Last modified 2010-03-17 10:02:41 by Eric Scrivner>
 //
 // Description:
 //   Provides a set of unit-tests using Lemon for a simple factorial function.
+//
+// Copyright (c) 2010 Lemon Team
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software. If you use this software
+// in a product, an acknowledgment in the product documentation would be
+// appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such, and must not be
+// misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source
+// distribution.
 ///////////////////////////////////////////////////////////////////////////////
 #include "../lemon.h"
 
--- examples/test_suite_example.cpp
+++ examples/test_suite_example.cpp
@@ -1,10 +1,31 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Lemon Example Two: Test Suites
 //
-// Time-stamp: <Last modified 2010-02-27 16:02:54 by Eric Scrivner>
+// Time-stamp: <Last modified 2010-03-17 10:02:52 by Eric Scrivner>
 //
 // Description:
 //   Showcase one simple method for composing unit tests into suites
+//
+// Copyright (c) 2010 Lemon Team
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software. If you use this software
+// in a product, an acknowledgment in the product documentation would be
+// appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such, and must not be
+// misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source
+// distribution.
 ////////////////////////////////////////////////////////////////////////////////
 #include "../lemon.h"
 
