c065f38d0f446faea34ed1d3534369c9041f3b85
Eric Scrivner <zenogais@gmail.com>
2010-03-17 10:16:38 -0700

    Added copyright notices.

--- lemon.h
+++ lemon.h
@@ -1,10 +1,31 @@
 ///////////////////////////////////////////////////////////////////////////////
 // lemon.h - Contains the entire lemon unit testing framework
 //
-// Time-stamp: <Last modified 2010-03-05 17:54:58 by Eric Scrivner>
+// Time-stamp: <Last modified 2010-03-17 09:59:15 by Eric Scrivner>
 //
 // Description:
 //   A lightweight, minimal unit-testing framework based on Perl Test::More
+//
+// Copyright (c) 2010 Lemon Team
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software. If you use this software
+// in a product, an acknowledgment in the product documentation would be
+// appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such, and must not be
+// misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source
+// distribution.
 ///////////////////////////////////////////////////////////////////////////////
 #ifndef LEMON_H_
 #define LEMON_H_
@@ -68,6 +89,8 @@ namespace lemon {
   //
   // Policy-based class for doing testing
   template <class output_policy_t = lemon::output::cout>
+
+
   class test {
   public:
     ///////////////////////////////////////////////////////////////////////////
